66eec6b479f37cc56c2eaa35d6c4ed1664603927
Taehoon Kim <carpedm20@gmail.com>
2016-05-16 11:50:51 +0900

    graph_def explosion error fix. no more multiple creation of custom summary op

--- dqn/utils.py
+++ dqn/utils.py
@@ -11,10 +11,14 @@ def timeit(f):
     return result
   return timed
 
+summary = {}
 def inject_summary(writer, tag, value, step):
-  summary = tf.Summary(
-            value=[tf.Summary.Value(tag=tag, simple_value=value)])
-  writer.add_summary(summary, global_step=step)
+  if not summary.has_key(tag):
+    print " [*] Create summary_op for %s" % tag
+
+    summary[tag] = tf.Summary(
+        value=[tf.Summary.Value(tag=tag, simple_value=value)])
+  writer.add_summary(summary[tag], global_step=step)
 
 def get_time():
   return time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())
