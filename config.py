9db8d974f79a4b06b288f66ef13c5a011b8fa226
Taehoon Kim <carpedm20@gmail.com>
2016-06-24 08:44:18 +0900

    should reduce starting learning_rate by 10

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class AgentConfig(object):
   cnn_format = 'NCHW'
   discount = 0.99
   target_q_update_step = 1 * scale
-  learning_rate = 0.0025
+  learning_rate = 0.00025
   learning_rate_minimum = 0.00025
   learning_rate_decay = 0.96
   learning_rate_decay_step = 5 * scale
