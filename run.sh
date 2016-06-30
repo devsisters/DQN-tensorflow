2775e0d97e3bc303bdd8d06374df6134795d53c8
Taehoon Kim <carpedm20@gmail.com>
2016-06-30 11:52:43 +0900

    fix indexing of run.sh

--- run.sh
+++ run.sh
@@ -17,14 +17,23 @@ ps_hosts=`join , "${ps_ports[@]}"`
 worker_hosts=`join , "${worker_ports[@]}"`
 
 for ((i=0;i<$ps_num;i++)); do
-  echo_and_run python main.py \
+  echo_and_run CUDA_VISIBLE_DEVICES='' python main.py \
       --ps_hosts=$ps_hosts \
       --worker_hosts=$worker_hosts \
       --job_name=ps --task_index=$i "$@" \&
 done
 
+modular=$(($worker_num % $gpu_num))
+if [ $modular == 0 ]; then
+  add=0
+  denom=$(($worker_num/$gpu_num))
+else
+  add=1
+  denom=$(($worker_num/$gpu_num+1))
+fi
+
 for ((i=0;i<$worker_num;i++)); do
-  echo_and_run CUDA_VISIBLE_DEVICES=$(($i/($worker_num/$gpu_num))) python main.py \
+  echo_and_run CUDA_VISIBLE_DEVICES=$((($i+$add)/$denom)) python main.py \
       --ps_hosts=$ps_hosts \
       --worker_hosts=$worker_hosts \
       --job_name=worker --task_index=$i "$@" \&
